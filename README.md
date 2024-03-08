7a415d75bce012a3807a46f1e1c9116d051b1fc7
Richard Y. Hayashi <richardyhayashi@gmail.com>
2024-03-08 16:35:02 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Richard Y. Hayashi Portfolio
+
---
+++ next-app/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col justify-center items-center">
+      <h1 className="text-5xl">Hello World!</h1>  
+      <p>Under construction!</p>    
+    </main>
+  );
+}
