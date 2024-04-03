a636f63c76173511c7eb3da8b444cf8cd31b2422
Richard Y. Hayashi <richardyhayashi@gmail.com>
2024-04-03 11:27:15 +0900

    Modified home page and added README.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Richard Y. Hayashi Portfolio
 
+## Create Next.js App
+
+### yarn
+`$ yarn create next-app [<project-name>]`
+
+### npx
+
+`$ npx create-next-app@latest [<project-name>]`
--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center">
-      <h1 className="text-5xl">Hello World!</h1>  
+      <h1 className="text-5xl">Richard Y. Hayashi</h1>
+      <h2 className="text-3xl">Portfolio</h2>  
       <p>Under construction!</p>    
     </main>
   );
